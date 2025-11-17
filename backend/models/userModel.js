7ebfa5977a2473e5e0f51d5f6f4b6dbf585fb505
Jnanaprasad1903 <jnanaprasadjp19@gmail.com>
2025-11-17 21:45:47 +0530

    initial commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+export default mongoose.model('User', userSchema);
