fdf971fda5e7db56889261e64a5123a4b4f5573b
Jnanaprasad1903 <jnanaprasadjp19@gmail.com>
2025-11-19 22:09:32 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+#port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
